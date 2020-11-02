afa41c24256c406a6d0b5e75f5599da03eeef62f
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-02 14:22:57 -0300

    :wrench: chore: admin routes file added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const libraryRoutes = require("./routes/library");
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
@@ -11,10 +13,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "app", "views"));
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 // set static files path to express method argument
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 // routes
 app.use(libraryRoutes);
+app.use("/admin", adminRoutes);
 
 module.exports = app;
