fa580624dad53a4b8ce766d638fa88196fbbb8fb
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-02 14:17:32 -0300

    :construction: feat: smooth scroll script added

---
+++ public/scripts/smooth-scroll.js
@@ -0,0 +1,20 @@
+const SmoothScroll = (seletor) => {
+  const linksInternos = document.querySelectorAll(seletor);
+
+  const scrollToSection = (event) => {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+};
+
+SmoothScroll("a[href^='#']");
